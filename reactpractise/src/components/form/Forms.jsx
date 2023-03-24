4934ed58edfec67dd253af2449654b33ad4bce0a
Dinesh-Jadhav-15 <113357119+Dinesh-Jadhav-15@users.noreply.github.com>
2023-03-24 09:22:59 +0000

    Added React hooks and inputs

--- reactpractise/src/components/form/Forms.jsx
+++ reactpractise/src/components/form/Forms.jsx
@@ -1,69 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Forms.css";
 
 function Forms() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function Register(event) {
+    event.preventDefault();
+    console.log(
+      "Registration Details : " + name + " " + email + " " + password + " "
+    );
+  }
+
   return (
-    <div class="main-block">
-      <h1>Registration</h1>
-      <form action="/">
-        <hr />
-        <div class="account-type">
+    <div className="main-wrapper">
+      <div className="main-block">
+        <h1>Registration</h1>
+
+        <form onSubmit={() => Register(event)}>
+          <hr />
+          <div className="account-type">
+            <input
+              type="radio"
+              value="none"
+              id="radioOne"
+              name="account"
+              checked
+            />
+            <label for="radioOne" className="radio">
+              Personal
+            </label>
+            <input type="radio" value="none" id="radioTwo" name="account" />
+            <label for="radioTwo" className="radio">
+              Company
+            </label>
+          </div>
+          <hr />
+          <label id="icon" for="name">
+            <i className="fas fa-envelope"></i>
+          </label>
           <input
-            type="radio"
-            value="none"
-            id="radioOne"
-            name="account"
-            checked
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
-          <label for="radioOne" class="radio">
-            Personal
+          <label id="icon" for="name">
+            <i className="fas fa-user"></i>
           </label>
-          <input type="radio" value="none" id="radioTwo" name="account" />
-          <label for="radioTwo" class="radio">
-            Company
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <label id="icon" for="name">
+            <i className="fas fa-unlock-alt"></i>
           </label>
-        </div>
-        <hr />
-        <label id="icon" for="name">
-          <i class="fas fa-envelope"></i>
-        </label>
-        <input type="text" name="name" id="name" placeholder="Email" required />
-        <label id="icon" for="name">
-          <i class="fas fa-user"></i>
-        </label>
-        <input type="text" name="name" id="name" placeholder="Name" required />
-        <label id="icon" for="name">
-          <i class="fas fa-unlock-alt"></i>
-        </label>
-        <input
-          type="password"
-          name="name"
-          id="name"
-          placeholder="Password"
-          required
-        />
-        <hr />
-        <div class="gender">
-          <input type="radio" value="none" id="male" name="gender" checked />
-          <label for="male" class="radio">
-            Male
-          </label>
-          <input type="radio" value="none" id="female" name="gender" />
-          <label for="female" class="radio">
-            Female
-          </label>
-        </div>
-        <hr />
-        <div class="btn-block">
-          <p>
-            By clicking Register, you agree on our{" "}
-            <a href="https://www.google.com/">Privacy Policy for Google.com</a>.
-          </p>
-          <button type="submit" href="/">
-            Submit
-          </button>
-        </div>
-      </form>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <hr />
+          <div className="gender">
+            <input type="radio" value="none" id="male" name="gender" checked />
+            <label for="male" className="radio">
+              Male
+            </label>
+            <input type="radio" value="none" id="female" name="gender" />
+            <label for="female" className="radio">
+              Female
+            </label>
+          </div>
+          <hr />
+          <div className="btn-block">
+            <p>
+              By clicking Register, you agree on our
+              <a href="https://www.google.com/" target="_blank">
+                Privacy Policy for Google.com
+              </a>
+              .
+            </p>{" "}
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
