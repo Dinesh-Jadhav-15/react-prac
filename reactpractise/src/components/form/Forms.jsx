cfe444a4cf2607367bcb9f2cc46e7a9525669673
Dinesh-Jadhav-15 <113357119+Dinesh-Jadhav-15@users.noreply.github.com>
2023-03-24 05:50:09 +0000

    Form UI is complete

--- reactpractise/src/components/form/Forms.jsx
+++ reactpractise/src/components/form/Forms.jsx
@@ -1,28 +1,70 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import React from "react";
+import "./Forms.css";
 
 function Forms() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <div class="main-block">
+      <h1>Registration</h1>
+      <form action="/">
+        <hr />
+        <div class="account-type">
+          <input
+            type="radio"
+            value="none"
+            id="radioOne"
+            name="account"
+            checked
+          />
+          <label for="radioOne" class="radio">
+            Personal
+          </label>
+          <input type="radio" value="none" id="radioTwo" name="account" />
+          <label for="radioTwo" class="radio">
+            Company
+          </label>
+        </div>
+        <hr />
+        <label id="icon" for="name">
+          <i class="fas fa-envelope"></i>
+        </label>
+        <input type="text" name="name" id="name" placeholder="Email" required />
+        <label id="icon" for="name">
+          <i class="fas fa-user"></i>
+        </label>
+        <input type="text" name="name" id="name" placeholder="Name" required />
+        <label id="icon" for="name">
+          <i class="fas fa-unlock-alt"></i>
+        </label>
+        <input
+          type="password"
+          name="name"
+          id="name"
+          placeholder="Password"
+          required
+        />
+        <hr />
+        <div class="gender">
+          <input type="radio" value="none" id="male" name="gender" checked />
+          <label for="male" class="radio">
+            Male
+          </label>
+          <input type="radio" value="none" id="female" name="gender" />
+          <label for="female" class="radio">
+            Female
+          </label>
+        </div>
+        <hr />
+        <div class="btn-block">
+          <p>
+            By clicking Register, you agree on our{" "}
+            <a href="https://www.google.com/">Privacy Policy for Google.com</a>.
+          </p>
+          <button type="submit" href="/">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
